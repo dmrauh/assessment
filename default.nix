8790997932837d8bd5caa8765b7d5a0b4bee56f9
Dominik Michael Rauh <dmrauh@posteo.de>
2020-01-31 14:18:36 +0100

    Reformat default.nix

--- default.nix
+++ default.nix
@@ -1,15 +1,12 @@
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 
 stdenv.mkDerivation rec {
   name = "assessment-env";
 
   buildInputs = [
-    (python3.withPackages (ps: [
-      ps.yapf
-      ps.click
-      ps.pep8
-    ]))
-    mypy
+    (python3.withPackages (ps: with ps; [ click ]))
+    python3Packages.mypy
+    python3Packages.yapf
   ];
 }
 
