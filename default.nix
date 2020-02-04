cb84743141d644f14aa4d7a72b67e00154835965
Dominik Michael Rauh <dmrauh@posteo.de>
2020-02-04 14:27:50 +0100

    Simplify default.nix

--- default.nix
+++ default.nix
@@ -3,10 +3,7 @@ with import <nixpkgs> { };
 stdenv.mkDerivation rec {
   name = "assessment-env";
 
-  buildInputs = [
-    (python3.withPackages (ps: with ps; [ click ]))
-    python3Packages.mypy
-    python3Packages.yapf
-  ];
+  buildInputs = [ (python3.withPackages (ps: with ps; [ click ])) ]
+    ++ (with python3Packages; [ ipython mypy yapf ]);
 }
 
