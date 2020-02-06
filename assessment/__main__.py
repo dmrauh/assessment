7d9f4af936a0a0bccd44add1761b5bd390b77679
Dominik Michael Rauh <dmrauh@posteo.de>
2020-02-06 21:18:37 +0100

    Package using setuptools

---
+++ assessment/__main__.py
@@ -0,0 +1,3 @@
+import assessment
+
+assessment.main()
--- default.nix
+++ default.nix
@@ -1,9 +1,20 @@
 with import <nixpkgs> { };
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   name = "assessment-env";
+  buildInputs = [
+    # System requirements.
+    readline
 
-  buildInputs = [ (python3.withPackages (ps: with ps; [ click ])) ]
-    ++ (with python3Packages; [ ipython mypy yapf ]);
+    # Python requirements (enough to get a virtualenv going).
+    (python3.withPackages (ps: with ps; [ click setuptools ]))
+  ] ++ (with python3Packages; [ virtualenv pip ]);
+  src = null;
+  shellHook = ''
+    # Allow the use of wheels.
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    # Augment the dynamic linker path
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${R}/lib/R/lib:${readline}/lib
+  '';
 }
-
