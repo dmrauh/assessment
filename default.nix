07b1d6c124afb6938d2d1f287282b543d7a4356f
Dominik Michael Rauh <dmrauh@posteo.de>
2020-02-08 15:49:18 +0100

    Fall back to venv based nix-shell

--- default.nix
+++ default.nix
@@ -1,9 +1,24 @@
 with import <nixpkgs> {};
-with pkgs.python3Packages;
 
-buildPythonPackage rec {
-  name = "assessment";
-  src = ./.;
-  propagatedBuildInputs = [ click ];
+stdenv.mkDerivation {
+  name = "assessment-env";
+  buildInputs = [
+    # System requirements.
+    readline
+
+    # Python requirements (enough to get a virtualenv going).
+    (python3.withPackages (ps: with ps; [click hypothesis] ))
+    python3Packages.virtualenv
+    python3Packages.pip
+    python3Packages.pytest
+  ];
+  src = null;
+  shellHook = ''
+    # Allow the use of wheels.
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    # Augment the dynamic linker path
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${R}/lib/R/lib:${readline}/lib
+  '';
 }
 
